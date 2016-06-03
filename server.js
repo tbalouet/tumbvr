8d2b04d004449daa2616cf4fcb52d1de9b29a1c5
Thomas Balouet <Thomas Balouet>
2016-06-03 15:29:59 +0200

    added parameters to corsproxy initialisation

--- server.js
+++ server.js
@@ -16,12 +16,39 @@ if(conf.error){
 
 //CORS PROXY to enable fetching images from other servers
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var port = process.env.PORT || 8080;
+var port = 8080;
+
+// Grab the blacklist from the command-line so that we can update the blacklist without deploying
+// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
+// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
+// use originWhitelist instead.
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+function parseEnvList(env) {
+  if (!env) {
+    return [];
+  }
+  return env.split(',');
+}
 
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+	originBlacklist: originBlacklist,
+	originWhitelist: originWhitelist,
+	requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: [
+		'cookie',
+		'cookie2',
+		// Strip Heroku-specific headers
+		'x-heroku-queue-wait-time',
+		'x-heroku-queue-depth',
+		'x-heroku-dynos-in-use',
+		'x-request-start',
+	],
+	redirectSameOrigin: true,
+	httpProxyOptions: {
+		// Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+		xfwd: false,
+	},
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
