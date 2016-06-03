761328c0104aaa3914480ce110b316edf7eb1daf
Thomas Balouet <Thomas Balouet>
2016-06-03 12:29:37 +0200

    trying to reset cors and tumblr functions

--- server.js
+++ server.js
@@ -14,25 +14,26 @@ if(conf.error){
 	return;
 }
 
-// //CORS PROXY to enable fetching images from other servers
-// var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-// var port = process.env.PORT || 8080;
+//CORS PROXY to enable fetching images from other servers
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 8080;
 
-// cors_proxy.createServer({
-//     originWhitelist: [], // Allow all origins
-//     requireHeader: ['origin', 'x-requested-with'],
-//     removeHeaders: ['cookie', 'cookie2']
-// }).listen(port, host, function() {
-//     console.log('Running CORS Anywhere on ' + host + ':' + port);
-// });
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
-// var key = process.env.TUMBLR_KEY || "YOUR_KEY_HERE";
-// if(key === "YOUR_KEY_HERE"){
-// 	console.error("You have to set your Tumblr API key to make the project work: https://www.tumblr.com/oauth/apps");
-// 	return;
-// }
+var key = process.env.TUMBLR_KEY || "YOUR_KEY_HERE";
+if(key === "YOUR_KEY_HERE"){
+	console.error("You have to set your Tumblr API key to make the project work: https://www.tumblr.com/oauth/apps");
+	return;
+}
+
 //Tumblr Client to retrieve images from accounts
-var tumbClient = tumblr.createClient({ consumer_key: "testfalsekey" });
+var tumbClient = tumblr.createClient({ consumer_key: key });
 
 app.set('port', (process.env.PORT || conf.port));
 app.use(express.static(__dirname + '/public', {maxAge : conf.maxAge}));
