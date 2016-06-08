03de9ee9f14db179085b23bf124db2bc448d932c
Thomas Balouet <Thomas Balouet>
2016-06-08 18:43:46 +0200

    leaving heroku, adding routing in server

--- server.js
+++ server.js
@@ -14,19 +14,34 @@ if(conf.error){
 	return;
 }
 
+//CORS PROXY to enable fetching images from other servers
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 8080;
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
 var key = process.env.TUMBLR_KEY || "YOUR_KEY_HERE";
 if(key === "YOUR_KEY_HERE"){
 	console.error("You have to set your Tumblr API key to make the project work: https://www.tumblr.com/oauth/apps");
 	return;
 }
-
 //Tumblr Client to retrieve images from accounts
 var tumbClient = tumblr.createClient({ consumer_key: key });
 
-app.set('port', (process.env.PORT || conf.port));
-app.use(express.static(__dirname + '/public', {maxAge : 1}));
 
-app.get('/*', function(req, res){
+app.use(express.static(__dirname + '/public', {maxAge : conf.maxAge}));
+
+app.get('/', function(req, res){
+	res.render('home.ejs', {aFrameFile : conf.aFrameFile});
+});
+
+app.get('/tumbvr', function(req, res){
 	if(req.params[0] === ""){
 		req.params[0] = "aframevr";
 	}
@@ -36,6 +51,5 @@ app.get('/*', function(req, res){
 	});
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(conf.port);
+console.log('Listening on port ' + conf.port);
